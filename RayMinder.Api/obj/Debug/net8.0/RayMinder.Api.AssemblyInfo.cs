db011e043a9586ff0f578d5bf434ed2d6566d330
jofelguevarra <jag063@student.usc.edu.au>
2025-10-30 07:35:25 +1000

    commit

--- RayMinder.Api/obj/Debug/net8.0/RayMinder.Api.AssemblyInfo.cs
+++ RayMinder.Api/obj/Debug/net8.0/RayMinder.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RayMinder.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0f378504e8c5e094177c5282e0fded359adf467b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+286d682ba303d57175ea3d7b1b4a95aac9515549")]
 [assembly: System.Reflection.AssemblyProductAttribute("RayMinder.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RayMinder.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
